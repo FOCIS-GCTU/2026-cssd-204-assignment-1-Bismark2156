7cb499febed7cc7f0c00c8ba8b824f2565a472b3
Bismark2156 <2425402943@live.gctu.edu.gh>
2026-05-31 18:58:57 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File: initials.py
+# Description: Print out my initials using python.
+# Assignment Number: 2
+#
+# Name: OFORI BISMARK BOATENG
+# STUDENT ID: 2425402943
+# Email:    2425402943@live.gctu.edu.gh
+# Grader: MR. AUGUSTUS BUCKMAN
+#
+# On my honor, OFORI BISMARK BOATENG, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Print stylized initials
 
+    print()
+    print("...OBB")
+    print()
+
+    print("...0OOOOOOOOOO0...  ...BBBBBBBBBBB...  ...BBBBBBBBBB...")
+    print("...0OO......OO0...  ...BBB......BB...  ...BB.......BB...")
+    print("...0OO......OO0...  ...BBB......BBB...  ...BBB......BBB...")
+    print("...0OO......OO0...  ...BBB......BBB...  ...BBB......BBB...")
+    print("...0OO......OO0...  ...BBBBBBBBBBB...  ...BBBBBBBBBBB...")
+    print("...0OO......OO0...  ...BBB......BB...  ...BBB.......BB...")
+    print("...0OO......OO0...  ...BBB......BBB...  ...BBB......BBB...")
+    print("...0OO......OO0...  ...BBB......BBB...  ...BBB......BBB...")
+    print("...0OO......OO0...**...BBB......BB...**...BBB.......BB...**")
+    print("...0OOOOOOOOOO0...**...BBBBBBBBBBB...**...BBBBBBBBBBB...**")
+
+    print()
 
 
 main()
